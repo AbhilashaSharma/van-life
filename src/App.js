91298c40c564a114cf488808896ea5698b2498c4
AbhilashaSharma <abhilashasharma75@gmail.com>
2026-02-24 12:40:38 +0800

    Adding files and gitignore

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import "./App.css";
+import About from "./Components/About";
+import Home from "./Components/Home";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <header>
+        <Link className="site-logo" to="/">
+          #VanLife
+        </Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
